90cf603784266399f0dee0d3d72c3803b249789f
Martti Laanesaar <laanesaar.martti@khk.ee>
2021-04-06 09:32:55 +0300

    Defined background color

--- game.py
+++ game.py
@@ -6,12 +6,19 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("PyGame")
+    # background color
+    bg_color = (238,238,228)
+
+
+
 
     while True:
         # control keyboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # add screen background
+        screen.fill(bg_color)
         # display the last screen
         pygame.display.flip()
 # test game
